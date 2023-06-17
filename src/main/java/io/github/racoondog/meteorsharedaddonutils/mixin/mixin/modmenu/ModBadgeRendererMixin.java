e8d1ed183e6cb121ef26027a490daa7975669126
ridglef <126202739+ridglef@users.noreply.github.com>
2023-06-17 19:23:01 -0400

    [skip ci] 1.20.1 (#2)

--- src/main/java/io/github/racoondog/meteorsharedaddonutils/mixin/mixin/modmenu/ModBadgeRendererMixin.java
+++ src/main/java/io/github/racoondog/meteorsharedaddonutils/mixin/mixin/modmenu/ModBadgeRendererMixin.java
@@ -5,6 +5,7 @@ import com.terraformersmc.modmenu.util.mod.ModBadgeRenderer;
 import io.github.racoondog.meteorsharedaddonutils.utils.ModMetaUtils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
@@ -18,10 +19,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = ModBadgeRenderer.class, remap = false)
 public abstract class ModBadgeRendererMixin {
     @Shadow protected Mod mod;
-    @Shadow public abstract void drawBadge(MatrixStack matrices, OrderedText text, int outlineColor, int fillColor, int mouseX, int mouseY);
+    @Shadow public abstract void drawBadge(DrawContext context, OrderedText text, int outlineColor, int fillColor, int mouseX, int mouseY);
 
     @Inject(method = "draw", at = @At("TAIL"))
-    private void addMeteorBadge(MatrixStack matrices, int mouseX, int mouseY, CallbackInfo ci) {
-        if (ModMetaUtils.hasModBadge(this.mod, "meteor")) drawBadge(matrices, Text.literal("Meteor").asOrderedText(), 0xFF913DE2, 0x99913DE2, mouseX, mouseY);
+    private void addMeteorBadge(DrawContext context, int mouseX, int mouseY, CallbackInfo ci) {
+        if (ModMetaUtils.hasModBadge(this.mod, "meteor")) drawBadge(context, Text.literal("Meteor").asOrderedText(), 0xFF913DE2, 0x99913DE2, mouseX, mouseY);
     }
 }
